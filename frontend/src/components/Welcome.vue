6814f10e8f178f7f2f0690ecd6e40ee614e0f8e3
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2025-06-18 12:46:20 +0530

    updated padding for leadership cards

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -39,10 +39,10 @@
 
 
     <!-- Content Section -->
-    <section class="px-4 md:px-8 py-6 max-w-7xl mx-auto">
+    <section class="px-4 md:px-8 py-2 max-w-7xl mx-auto mb-2">
       <div class="flex flex-col lg:flex-row gap-8">
         <!-- Leadership Cards -->
-        <div class="w-full lg:w-1/3 space-y-4 ">
+        <div class="w-full lg:w-1/3 space-y-2">
           <div
             class="bg-white rounded-2xl p-2 shadow-xl hover:shadow-2xl transition-all duration-300 group relative overflow-hidden">
             <div
